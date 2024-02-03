9398169c2adb256eae25d9f349c0bec54bc95bb5
Reshma62 <mernstack2024@gmail.com>
2024-02-03 23:29:03 +0600

    upate

--- src/utils/baseUrlPublic.jsx
+++ src/utils/baseUrlPublic.jsx
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 console.log("Token:", token); // Check if token is undefined or null
 const axiosPublic = axios.create({
   // baseURL: "http://localhost:8000/api/v1",
-  baseURL: "https://simple-design-server.vercel.app",
+  baseURL: "https://simple-design-server.vercel.app/api/v1",
 
   headers: {
     authorization: token,
